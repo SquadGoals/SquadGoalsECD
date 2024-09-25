04753e3c626521e24acbf921573ec5cb0429b886
Zimkhitha Nongomaza <87832119+ZimkhithaNongomaza@users.noreply.github.com>
2024-09-25 09:56:07 +0200

    Update Calculations.js

--- Calculations.js
+++ Calculations.js
@@ -68,5 +68,31 @@ var weekend3 = ["weekend3"];
  
  // Add event listener to the button
  document.getElementById('calculateBtn').addEventListener('click', total);
+
+ //code for the modal 
  
+ var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("calculateBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
